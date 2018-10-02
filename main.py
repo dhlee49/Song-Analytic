7c9adbeb17bff91601ac01b376f14cc97254365c
DH <l.donghoon49@gmail.com>
2018-10-02 11:11:20 -0400

    A1 EECS4415

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import songs
+
+from os import path
+
+__dirpath__ = path.dirname(__file__)
+
+def main():
+  outputpath = path.join(__dirpath__, '../outputs')
+  if __name__ == "__main__":
+  main()
